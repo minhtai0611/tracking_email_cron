13ff17761068b961f39b5b33409b659e1d5e3ae5
minhtai0611 <minhtainth06111999@gmail.com>
2024-11-09 09:29:54 +0700

    update

--- tracking_email.py
+++ tracking_email.py
@@ -9,17 +9,17 @@ load_dotenv()
 SCOPES = ['https://www.googleapis.com/auth/spreadsheets']
 print(os.getenv('SPREADSHEET_ID'), os.getenv('TYPE'), os.getenv('PROJECT_ID'), os.getenv('PRIVATE_KEY_ID'), os.getenv('PRIVATE_KEY'), os.getenv('CLIENT_EMAIL'), os.getenv('CLIENT_ID'), os.getenv('AUTH_URI'), os.getenv('TOKEN_URI'), os.getenv('AUTH_PROVIDER_CERT_URL'), os.getenv('CLIENT_CERT_URL'), os.getenv('UNIVERSE_DOMAIN'))
 credentials = service_account.Credentials.from_service_account_info({
-  "type": f"{os.getenv('TYPE')}",
-  "project_id": f"{os.getenv('PROJECT_ID')}",
-  "private_key_id": f"{os.getenv('PRIVATE_KEY_ID')}",
-  "private_key": f"{os.getenv('PRIVATE_KEY').replace("\\n", "\n")}",
-  "client_email": f"{os.getenv('CLIENT_EMAIL')}",
-  "client_id": f"{os.getenv('CLIENT_ID')}",
-  "auth_uri": f"{os.getenv('AUTH_URI')}",
-  "token_uri": f"{os.getenv('TOKEN_URI')}",
-  "auth_provider_x509_cert_url": f"{os.getenv('AUTH_PROVIDER_CERT_URL')}",
-  "client_x509_cert_url": f"{os.getenv('CLIENT_CERT_URL')}",
-  "universe_domain": f"{os.getenv('UNIVERSE_DOMAIN')}"
+  "type": os.getenv('TYPE'),
+  "project_id": os.getenv('PROJECT_ID'),
+  "private_key_id": os.getenv('PRIVATE_KEY_ID'),
+  "private_key": os.getenv('PRIVATE_KEY').replace("\\n", "\n"),
+  "client_email": os.getenv('CLIENT_EMAIL'),
+  "client_id": os.getenv('CLIENT_ID'),
+  "auth_uri": os.getenv('AUTH_URI'),
+  "token_uri": os.getenv('TOKEN_URI'),
+  "auth_provider_x509_cert_url": os.getenv('AUTH_PROVIDER_CERT_URL'),
+  "client_x509_cert_url": os.getenv('CLIENT_CERT_URL'),
+  "universe_domain": os.getenv('UNIVERSE_DOMAIN')"
 })
 spreadsheet_service = build('sheets', 'v4', credentials=credentials)
 
